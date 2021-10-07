fe3fc4b0b8ec48ab55ba440d2b3b709b68941918
Siva OS <syskantechnosoft@users.noreply.github.com>
2021-10-08 03:11:35 +0530

    Add files via upload

---
+++ Day9JDBCAdv/src/com/revature/jdbc/main/ASCIITable.java
@@ -0,0 +1,13 @@
+package com.revature.jdbc.main;
+
+public class ASCIITable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < 1025; i++)
+			System.out.println(i + " =" + (char) i);
+		
+		System.out.println("σ±╖╕╣║╗╝╜╛┐└┴┬├────┼╞──┼┼");
+	}
+
+}
